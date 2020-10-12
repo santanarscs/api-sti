daed4d8da4a6bd20e838c68c0f48ca416de5ab4c
Raphael Santana <raphaelstn@gmail.com>
2020-10-12 12:24:43 -0300

    chore: register routes

---
+++ src/routes/index.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+
+import sectionsRouter from '../modules/sections/infra/http/routes/sections.routes';
+import graduationsRouter from '../modules/users/infra/http/routes/graduations.routes';
+import specialtiesRouter from '../modules/users/infra/http/routes/specialties.routes';
+import boardsRouter from '../modules/users/infra/http/routes/boards.routes';
+
+import usersRouter from '../modules/users/infra/http/routes/users.routes';
+
+const routes = Router();
+
+routes.use('/sections', sectionsRouter);
+routes.use('/graduations', graduationsRouter);
+routes.use('/specialties', specialtiesRouter);
+routes.use('/boards', boardsRouter);
+routes.use('/users', usersRouter);
+
+export default routes;
