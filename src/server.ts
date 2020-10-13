b879d3192214b49e110746cc8a96d6995f2ae171
santanarscs <raphaelstn@gmail.com>
2020-10-13 16:09:40 -0300

    feat: configure cors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 import './database';
 import './container';
@@ -7,6 +8,8 @@ import './container';
 const PORT = 3333;
 const HOST = '0.0.0.0';
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
