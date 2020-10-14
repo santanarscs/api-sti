24b84b7079a894f86ea4e2418033aa777af6629a
santanarscs <raphaelstn@gmail.com>
2020-10-14 16:49:47 -0300

    chore(equipaments): register new routes

--- src/modules/equipaments/infra/http/routes/equipaments.routes.ts
+++ src/modules/equipaments/infra/http/routes/equipaments.routes.ts
@@ -4,6 +4,8 @@ import ListEquipamentsService from '../../../services/ListEquipamentsService';
 import CreateEquipamentService from '../../../services/CreateEquipamentService';
 import movimentationRoutes from './movimentations.routes';
 import FindEquipamentService from '../../../services/FindEquipamentService';
+import UpdateEquipamentService from '../../../services/UpdateEquipamentService';
+import DeleteEquipamentService from '../../../services/DeleteEquipamentService';
 
 const equipamentsRoutes = Router();
 
@@ -25,6 +27,28 @@ equipamentsRoutes.post('/', async (request: Request, response: Response) => {
   const equipament = await createEquipament.execute(request.body);
   return response.json(equipament);
 });
+
+equipamentsRoutes.put('/:id', async (request: Request, response: Response) => {
+  const { id } = request.params;
+
+  const updateEquipament = container.resolve(UpdateEquipamentService);
+  const equipament = await updateEquipament.execute({
+    ...request.body,
+    id,
+  });
+  return response.json(equipament);
+});
+
+equipamentsRoutes.delete(
+  '/:id',
+  async (request: Request, response: Response) => {
+    const { id } = request.params;
+
+    const deleteEquipament = container.resolve(DeleteEquipamentService);
+    await deleteEquipament.execute(id);
+    return response.status(204).send();
+  },
+);
 equipamentsRoutes.use('/movimentations', movimentationRoutes);
 
 export default equipamentsRoutes;
