33e59aa9b6a0028fc000918d8831877bdd92c7f9
santanarscs <raphaelstn@gmail.com>
2020-10-20 15:19:53 -0300

    feat: add global error

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import path from 'path';
 import cors from 'cors';
 import routes from './routes';
 import './database';
 import './container';
+import AppError from './AppError';
 
 const PORT = 3333;
 const HOST = '0.0.0.0';
@@ -22,6 +23,23 @@ app.use(routes);
 app.get('/', (request, response) =>
   response.json({ message: 'Server online' }),
 );
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  // eslint-disable-next-line
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(PORT, HOST, () => {
   console.log('⚡️⚡️⚡️The father is ON!!!');
