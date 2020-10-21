a135807870690a8b8b90191324e12eea793838a5
santanarscs <raphaelstn@gmail.com>
2020-10-21 16:14:48 -0300

    feat(orders): define inital repository

---
+++ src/modules/orders/repositories/IOrdersRepository.ts
@@ -0,0 +1,16 @@
+import ICreateOrderDTO from '../dtos/ICreateOrderDTO';
+import IOrder from '../models/IOrder';
+
+interface IFindOrders {
+  page: number;
+  limit: number;
+  queryName?: string;
+}
+
+export interface IOrdersRepository {
+  findById(id: string): Promise<IOrder | undefined>;
+  create(data: ICreateOrderDTO): Promise<IOrder>;
+  list(data: IFindOrders): Promise<[IOrder[], number]>;
+  delete(id: string): Promise<void>;
+  save(equipament: IOrder): Promise<IOrder>;
+}
