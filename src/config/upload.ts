4e972f721eb2d3f626dcf1cb3c557c11a5dca50f
Raphael Santana <raphaelstn@gmail.com>
2020-10-30 12:23:37 -0300

    feat(providers): add storage provider

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,16 +1,50 @@
-import multer from 'multer';
 import { resolve } from 'path';
+import crypto from 'crypto';
+import multer from 'multer';
 
-const tmpFolder = resolve(__dirname, '..', '..', 'uploads');
+const tmpFolder = resolve(__dirname, '..', '..', 'tmp');
+
+interface IUploadConfig {
+  driver: 's3' | 'disk';
+
+  tmpFolder: string;
+  uploadsFolder: string;
+
+  multer: {
+    storage: multer.StorageEngine;
+  };
+
+  config: {
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    disk: {};
+    aws: {
+      bucket: string;
+    };
+  };
+}
 
 export default {
-  directory: tmpFolder,
-  storage: multer.diskStorage({
-    destination: tmpFolder,
-    filename: (request, file, cb) => {
-      const filename = `${Date.now()}-${file.originalname}`;
+  driver: process.env.STORAGE_DRIVER,
 
-      cb(null, filename);
+  tmpFolder,
+  uploadsFolder: resolve(tmpFolder, 'uploads'),
+
+  multer: {
+    storage: multer.diskStorage({
+      destination: tmpFolder,
+      filename(req, file, callback) {
+        const fileHash = crypto.randomBytes(10).toString('HEX');
+        const fileName = `${fileHash}-${file.originalname}`;
+
+        return callback(null, fileName);
+      },
+    }),
+  },
+
+  config: {
+    disk: {},
+    aws: {
+      bucket: 'santanarscs',
     },
-  }),
-};
+  },
+} as IUploadConfig;
--- src/container/providers/index.ts
+++ src/container/providers/index.ts
@@ -1,1 +1,2 @@
 import './LdapProvider';
+import './StorageProvider';
